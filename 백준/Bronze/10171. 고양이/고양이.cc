8ae9acff8901e7e0a1576fc3e684e25aff6ab281
konel__ <126758198+konel0315@users.noreply.github.com>
2025-11-16 19:38:32 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+
+	cout <<"\\    /\\" << "\n"<<" )  ( ')" << "\n" <<"(  /  )" << "\n"<<" \\(__)|" << "\n";
+	
+	
+}
+
